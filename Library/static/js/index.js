92826466b2f99967c44ec8f154e8e4f68bbc401d
Khalil2506 <113655824+Khalil2506@users.noreply.github.com>
2023-02-22 12:18:50 -0500

    home almost finished

--- Library/static/js/index.js
+++ Library/static/js/index.js
@@ -24,27 +24,39 @@ window.addEventListener("scroll", function() {
     }
   }
 });
-// codigo maquina
-const maquina = document.getElementById('maquina');
-const maquinaescribir = (text = '', tiempo = 200, etiqueta = '') =>{
-    let arrayCaracteres = text.split('');
-    etiqueta.innerHTML = '';
-    let i = 0;
-    let j = text.length;
-    let escribir = setInterval(function(){
-        if (i===arrayCaracteres.length){
-            etiqueta.innerHTML = text.substring(0,j);
-            j--
-            if (j===0){
-                etiqueta.innerHTML = '';
-                i= 0 ;
-                j = text.length;
-            }
-        } else{
-            etiqueta.innerHTML += arrayCaracteres[i];
-            i++
-        }
-    }, tiempo)
-}
 
-maquinaescribir("Developer",150,maquina);
+// codigo maquina
+//const maquina = document.getElementById('maquina');
+//const maquinaescribir = (text = '', tiempo = 200, etiqueta = '') =>{
+//    let arrayCaracteres = text.split('');
+//    etiqueta.innerHTML = '';
+//    let i = 0;
+//    let j = text.length;
+//    let escribir = setInterval(function(){
+//        if (i===arrayCaracteres.length){
+//            etiqueta.innerHTML = text.substring(0,j);
+//            j--
+//            if (j===0){
+//                etiqueta.innerHTML = '';
+//                i= 0 ;
+//                j = text.length;
+//            }
+//        } else{
+//            etiqueta.innerHTML += arrayCaracteres[i];
+//            i++
+//        }
+//    }, tiempo)
+//}
+//
+//maquinaescribir("Developer",150,maquina);
+const links = document.querySelectorAll('a[href^="#"]');
+// agrega un evento de clic a cada enlace
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
